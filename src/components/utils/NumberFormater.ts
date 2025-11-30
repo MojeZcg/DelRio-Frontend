ea4b47006f23a30a6d33c0c660759fa80e6755c2
Walter J Montenegro <jesusmontenegro941@gmail.com>
2025-11-30 17:06:08 -0300

    fix: eliminar comentarios innecesarios en NumberFormater

--- src/components/utils/NumberFormater.ts
+++ src/components/utils/NumberFormater.ts
@@ -2,9 +2,9 @@ export default function formatearNumero(num: string) {
   // Asegura que no tenga espacios ni símbolos
   const limpio = num.replace(/\D/g, "");
 
-  const pais = limpio.slice(0, 2); // 54
-  const area = limpio.slice(2, 5); // 261
-  const resto = limpio.slice(5); // 5703852
+  const pais = limpio.slice(0, 2);
+  const area = limpio.slice(2, 5);
+  const resto = limpio.slice(5);
 
   return `+${pais} ${area} ${resto}`;
 }
