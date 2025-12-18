2affe1fd2a717d96e25de38c731debbcd8a88540
Walter Montenegro <jesusmontenegro941@gmail.com>
2025-12-18 14:12:16 -0300

    fix: py resuelto

--- src/components/mainpage/HeroImage.tsx
+++ src/components/mainpage/HeroImage.tsx
@@ -21,7 +21,7 @@ export default function HeroCarousel() {
   const [loadedImages, setLoadedImages] = useState<Record<number, boolean>>({});
 
   return (
-    <div className="hidden w-160 justify-center py-10 md:flex">
+    <div className="hidden w-160 justify-center md:flex">
       <Carousel
         className="w-full"
         opts={{ loop: true }}
